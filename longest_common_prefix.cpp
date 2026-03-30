81d0777fdfb1cdf65c57ff7ba6e9c09d54fd531f
rahul2k77-ui <rahulsharma2K77@gmail.com>
2026-03-30 23:53:11 +0530

    longest common prefix

--- longest_common_prefix.cpp
+++ longest_common_prefix.cpp
@@ -1,3 +1,8 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string.
+*/
+
+
 #include <iostream>
 #include <vector>
 #include <string>
