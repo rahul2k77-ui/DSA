343d3600b178c1f2a67bab85ecacea4959f8b50a
rahul_2k77 <rahulsharma2k77@gmail.com>
2026-04-27 23:56:17 +0530

    Solve Maximum Depth of Binary Tree

---
+++ maximumdepthof_binarytree_easy.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        return 1 + std::max(leftDepth, rightDepth);
+    }
+};
+
+void freeTree(TreeNode* root) {
+    if (root == nullptr) return;
+    freeTree(root->left);
+    freeTree(root->right);
+    delete root;
+}
+
+int main() {
+
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    Solution sol;
+    int depth = sol.maxDepth(root);
+
+    std::cout << "Maximum Depth of the tree is: " << depth << "\n";
+
+    freeTree(root);
+
+    return 0;
+}
